ce90449bc6e13f4ff4664b38a1f225436a76fbf6
Somnath Das <somnath8das@gmail.com>
2025-07-19 12:13:49 +0530

    product

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import HomeCarousel from "@/components/Screens/home/HomeCarousel";
 import { HomeSuggestions } from "@/components/Screens/home/HomeSuggestions";
+import { ProductDealCard } from "@/components/Screens/home/ProductDealCard";
 import { DeliveryLocation } from "@/components/Shared/DeliveryLocation";
 import { setSession } from "@/store/authSlice";
 import { RootState } from "@/store/store";
 import { supabase } from "@/supabase";
-import { ScrollView } from "react-native";
+import { Product } from "@/types/product";
+import { router } from "expo-router";
+import { ScrollView, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
@@ -15,8 +18,38 @@ export default function Home() {
     dispatch(setSession(null));
     await supabase.auth.signOut();
   };
+  const deals: Product[] = [
+    {
+      id: 1,
+      name: "Wireless Mouse",
+      amountInStock: 100,
+      currentPrice: 999,
+      previousPrice: 1299,
+      deliveryPrice: 40,
+      deliveryInDays: 2,
+      isAmazonChoice: true,
+      imageUrl: "https://uniquec.com/wp-content/uploads/235.jpg",
+      model3DUrl: null,
+    },
+    {
+      id: 2,
+      name: "Keyboard",
+      amountInStock: 50,
+      currentPrice: 1499,
+      previousPrice: 1799,
+      deliveryPrice: 60,
+      deliveryInDays: 3,
+      isAmazonChoice: false,
+      imageUrl: "https://m.media-amazon.com/images/I/61QxkJGYxeL.jpg",
+      model3DUrl: null,
+    },
+  ];
+  const onProductPress = ({ id }: Product) => {
+    router.push(`/product/${id}`);
+  };
   return (
     <ScrollView
+      scrollEnabled
       contentContainerStyle={{
         flex: 1,
       }}
@@ -24,6 +57,44 @@ export default function Home() {
       <DeliveryLocation />
       <HomeCarousel />
       <HomeSuggestions />
+      <View
+        style={{
+          backgroundColor: "white",
+          width: "100%",
+          padding: 20,
+          gap: 20,
+        }}
+      >
+        <Text
+          style={{
+            alignSelf: "flex-start",
+            fontFamily: "Amazon-Ember-Bold",
+            fontSize: 20,
+          }}
+        >
+          {session ? "Deals for you" : "Sign in for your best experience"}
+        </Text>
+        {session ? (
+          <View
+            style={{
+              justifyContent: "space-between",
+              gap: 30,
+              flexWrap: "wrap",
+              height: "21%",
+            }}
+          >
+            {deals.map((product) => (
+              <ProductDealCard
+                key={product.id}
+                product={product}
+                onPress={() => onProductPress(product)}
+              />
+            ))}
+          </View>
+        ) : (
+          <View></View>
+        )}
+      </View>
     </ScrollView>
   );
 }
---
+++ components/Screens/home/ProductDealCard.tsx
@@ -0,0 +1,70 @@
+import { Product } from "@/types/product";
+import { offPercentage } from "@/utils/number";
+import React from "react";
+import { Dimensions, Image, Pressable, Text, View } from "react-native";
+
+interface Props {
+  product: Product;
+  onPress: VoidFunction;
+}
+
+export function ProductDealCard({ product, onPress }: Props) {
+  return (
+    <Pressable onPress={onPress}>
+      <View
+        style={{
+          width: Dimensions.get("window").width / 2 - 40,
+          height: 160,
+          gap: 10,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            padding: 5,
+            width: "100%",
+            height: "80%",
+            borderRadius: 4,
+            backgroundColor: "rgba(0,0,0,0.1)",
+          }}
+        >
+          <Image
+            src={product.imageUrl ?? ""}
+            style={{ objectFit: "contain", width: "100%", height: "100%" }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              borderRadius: 4,
+              paddingHorizontal: 6,
+              paddingVertical: 4,
+              backgroundColor: "red",
+              color: "white",
+              fontWeight: "bold",
+              fontSize: 11,
+            }}
+          >
+            {offPercentage(product.currentPrice, product.previousPrice)}% off
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+              color: "red",
+              fontFamily: "Amazon-Ember",
+            }}
+          >
+            Limited deal
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+}
